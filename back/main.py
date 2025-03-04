2c2131c8086456da9b7aebba1c19a185322ae18f
Mindsky <romain.rondet@gmail.com>
2025-03-04 17:41:47 +0100

    back from scratch

--- back/main.py
+++ back/main.py
@@ -1,19 +1,6 @@
-from contextlib import asynccontextmanager
-
-from api.endpoints.user import router as user_router
-from db.session import close_db, init_db
 from fastapi import FastAPI
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_db()
-    yield
-    await close_db()
-
-
-app = FastAPI(lifespan=lifespan)
-app.include_router(user_router)
+app = FastAPI()
 
 
 @app.get("/")
