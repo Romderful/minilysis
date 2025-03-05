fe4ca3d9c8b47f8540f2ff735760dfede7c54ed4
Mindsky <romain.rondet@gmail.com>
2025-03-05 17:05:21 +0100

    call ok

--- front/app.vue
+++ front/app.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+import createClient from "openapi-fetch";
+import type { paths } from "./schema";
+
+const client = createClient<paths>({ baseUrl: "http://localhost:8000" });
+
+const status = ref();
+const err = ref();
+
+const call = async () => {
+  const { data, error } = await client.GET("/");
+  status.value = data;
+  err.value = error;
+};
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="call" />
+  </div>
+  <div>
+    <span v-if="status">{{ status }}</span>
+    <span v-else>{{ err }}</span>
   </div>
 </template>
