7b0cd0c821d1438f5dcda80457e20af763ee7d65
Mindsky <romain.rondet@gmail.com>
2025-03-05 17:10:37 +0100

    wip

--- front/app.vue
+++ front/app.vue
@@ -5,21 +5,18 @@ import type { paths } from "./schema";
 const client = createClient<paths>({ baseUrl: "http://localhost:8000" });
 
 const status = ref();
-const err = ref();
 
-const call = async () => {
-  const { data, error } = await client.GET("/");
+const getStatus = async () => {
+  const { data } = await client.GET("/");
   status.value = data;
-  err.value = error;
 };
 </script>
 
 <template>
   <div>
-    <button @click="call" />
+    <button @click="getStatus" />
   </div>
   <div>
-    <span v-if="status">{{ status }}</span>
-    <span v-else>{{ err }}</span>
+    <span>{{ status }}</span>
   </div>
 </template>
